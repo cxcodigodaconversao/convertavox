9975b8d5195e629eec679d5bd404393646392681
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2025-05-28 18:05:09 -0300

    Add files via upload

---
+++ src/main.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import ConvertaVoxApp from './ConvertaVoxApp'
+
+ReactDOM.createRoot(document.getElementById('root')).render(
+  <React.StrictMode>
+    <ConvertaVoxApp />
+  </React.StrictMode>
+)
