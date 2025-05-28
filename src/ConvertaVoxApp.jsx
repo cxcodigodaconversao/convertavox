59a95e1c3c597fb7ed65df9870522cb7e29202a0
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2025-05-28 18:04:09 -0300

    Delete src directory

--- src/ConvertaVoxApp.jsx
+++
@@ -1,9 +0,0 @@
-
-export default function ConvertaVoxApp() {
-  return (
-    <div style={{ padding: '2rem' }}>
-      <h1>🚀 <strong>ConvertaVox App</strong> funcionando com <strong>Vite + React!</strong></h1>
-      <p>Pré-visualização ativa com todos os componentes essenciais.</p>
-    </div>
-  );
-}
