e6871406a3729d55002969d22d22a719aebec47c
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2025-05-28 17:19:48 -0300

    Add files via upload

---
+++ src/ConvertaVoxApp.jsx
@@ -0,0 +1,8 @@
+export default function ConvertaVoxApp() {
+  return (
+    <div style={{ padding: "2rem", fontFamily: "Arial, sans-serif" }}>
+      <h1>🚀 ConvertaVox App funcionando com Vite + React!</h1>
+      <p>Pré-visualização ativa com todos os componentes essenciais.</p>
+    </div>
+  );
+}
