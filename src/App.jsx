1c3afcba4bcc2c06af3e97620f3cf103427afeb0
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2025-05-27 23:49:07 -0300

    Add files via upload

---
+++ src/App.jsx
@@ -0,0 +1,8 @@
+export default function App() {
+  return (
+    <div>
+      <h1>?? Convertavox funcionando com React + Vite no Netlify!</h1>
+      <p>Agora tudo está certo!</p>
+    </div>
+  );
+}
