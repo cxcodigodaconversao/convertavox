14609323b64c74df130b5d476b4a7021897243ea
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2025-05-30 17:53:52 -0300

    Delete src directory

--- src/main.jsx
+++
@@ -1,9 +0,0 @@
-
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import ConvertaVoxApp from './ConvertaVoxApp';
-
-const root = document.getElementById('root');
-if (root) {
-  ReactDOM.createRoot(root).render(<ConvertaVoxApp />);
-}
