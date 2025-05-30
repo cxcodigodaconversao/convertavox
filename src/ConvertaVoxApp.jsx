718c3e3bd85297aeede3e299392ae2e217802e37
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2025-05-30 18:27:41 -0300

    Add files via upload

---
+++ src/ConvertaVoxApp.jsx
@@ -0,0 +1,9 @@
+export default function ConvertaVoxApp() {
+  return (
+    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
+      <h1>📲 ConvertaVox™</h1>
+      <p>Radar Comportamental em Calls 1:1</p>
+      <p>Aplicativo funcionando com estrutura mínima.</p>
+    </div>
+  );
+}
