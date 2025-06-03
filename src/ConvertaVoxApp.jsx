d9be228820db4c74c71692b4f73941870d39d7ba
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2025-06-02 22:36:20 -0300

    Delete src directory

--- src/ConvertaVoxApp.jsx
+++
@@ -1,9 +0,0 @@
-export default function ConvertaVoxApp() {
-  return (
-    <div style={{ padding: '2rem', fontFamily: 'sans-serif' }}>
-      <h1>📲 ConvertaVox™</h1>
-      <p>Radar Comportamental em Calls 1:1</p>
-      <p>Aplicativo funcionando com estrutura mínima.</p>
-    </div>
-  );
-}
