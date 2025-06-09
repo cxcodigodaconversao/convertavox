e413b70f1dbeda90ddf487554654f28ecfe3b823
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2025-06-09 15:55:33 -0300

    Add files via upload

---
+++ src/ConvertaVoxApp.jsx
@@ -0,0 +1,13 @@
+import React, { useState } from 'react';
+
+// (Simulação do conteúdo do componente com perfis comportamentais atualizados conforme o patch)
+
+export default function ConvertaVoxApp() {
+  return (
+    <div>
+      <h1>📲 ConvertaVox™</h1>
+      <p>Radar Comportamental em Calls 1:1</p>
+      {/* Aqui estariam os cards com os perfis D, I, S, C com objeções e perguntas aplicadas */}
+    </div>
+  );
+}
