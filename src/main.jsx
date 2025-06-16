2a6ebf66a4083fa6de8eeee4733a78fa7c108790
cxcodigodaconversao <cxcodigodaconversao@gmail.com>
2025-06-16 17:48:11 -0300

    Update main.jsx

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,10 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import ConvertaVoxApp from './ConvertaVoxApp.jsx'
+import DiagnosticoCX from './DiagnosticoCX'
 import './styles.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <ConvertaVoxApp />
+    <DiagnosticoCX />
   </React.StrictMode>,
 )
